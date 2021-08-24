516bb0b128f7144090fada9572ac77ade9a04341
sourslaw <lawanscott@gmail.com>
2021-08-24 15:25:12 -0600

    updating frontend; app.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
-// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,7 +13,6 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
   context: authMiddleware,
 });
 
@@ -22,7 +21,6 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -30,7 +28,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
