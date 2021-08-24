9eefd9c51e6e2c093d80ff0ab95a98a44e18c747
Scott Lawan <scottlawan@Scotts-Mac-mini.local>
2021-08-24 13:26:20 -0600

    updating typeDefs

--- server/models/User.js
+++ server/models/User.js
@@ -32,6 +32,7 @@ const userSchema = new Schema(
   }
 );
 
+// set up pre-save middleware to create password
 // hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
@@ -42,6 +43,7 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// compare the incoming password with the hashed password
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,57 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookID: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+
+    addUser(username: String!, email: String!, password: String!): Auth
+
+    saveBook(input: BookInput): User
+
+    removeUser(bookId: String!): User
+
+    removeBook(book: String!): User
+  }
+
+`;
+
+module.exports = typeDefs;
